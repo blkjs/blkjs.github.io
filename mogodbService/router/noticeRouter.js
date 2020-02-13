5fe4c6028b28a3edccd548e37560902b83095d17
hf <1051011877@qq.com>
2020-02-14 00:23:08 +0800

    增加聊天头像，聊天云端记录

--- mogodbService/router/noticeRouter.js
+++ mogodbService/router/noticeRouter.js
@@ -30,7 +30,8 @@ var eventEmitter = new events.EventEmitter();
 const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 3001 });
- var arr = [];
+ var arr = [];//客户端信息
+ var messages = [];//用户消息,新用户接时把旧消息发送给新用户
  var data ='';
 setInterval(()=>{
 	if(arr.length !== data){
@@ -50,15 +51,21 @@ setInterval(()=>{
 	
 },2000)
  function websocket_add_listener(client_sock,req) {
-	
  	client_sock.on("message", function(data) {
- 		if(arr.indexOf(client_sock) === -1){//未找到则返回 -1
+		if(JSON.parse(data).message){
+			messages.push(data)//把消息存在服务器内存
+		}
+ 		if(arr.indexOf(client_sock) === -1){//未找到则返回 -1,第一次连接
 			client_sock.id=JSON.parse(data).join;
  			arr.push(client_sock);
+			messages.forEach(function each(data) {//新用户接时把旧消息发送给新用户
+				client_sock.send(data);
+			})
+			
  		}
 		console.log('当前在线人数:'+arr.length)
  		arr.forEach(function each(client) {
-			if(JSON.parse(data).modify_name){
+			if(JSON.parse(data).modify_name){//发送修改昵称消息给所有人
 				var n = arr.indexOf(client_sock);
 				arr[n].id=JSON.parse(data).modify_name
 				client.send(data);//发送修改昵称消息给所有人
@@ -66,19 +73,19 @@ setInterval(()=>{
 				arr.forEach(function each(client) {
 					list.push(client.id);
 				});
-				client.send(JSON.stringify({//更新在线人员列表
+				client.send(JSON.stringify({//推送在线人员列表
 						"num":arr.length,//在线人数
 						"list":list,//在线人员id
 				}));
 			}else if(client !== client_sock && client.readyState === WebSocket.OPEN) {//转发除自己外的所有消息
 				client.send(data);
- 		  }
+			}
  		});
  	});
  	// close事件
  	client_sock.on("close", function() {
 		//console.log(client_sock.id)
- 		var n = arr.indexOf(client_sock);
+ 		var n = arr.indexOf(client_sock);//找到关闭连接的客户端信息在arr数组中的位置
 		arr.forEach(function each(client) {
 			if(client !== client_sock && client.readyState === WebSocket.OPEN) {//发送退出者id消息,除退出者自己以外的所有人
 			client.send(JSON.stringify({
@@ -86,7 +93,7 @@ setInterval(()=>{
 			}));
 		  }
 		});
- 		var t = arr.splice(n,1)//从索引n开始删除1个元素
+ 		var t = arr.splice(n,1)//从索引n开始删除1个元素,删除关闭连接的客户端信息
 		//console.log(t[0].id)
  	});
  	 
